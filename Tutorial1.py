4c494a5f19f3f79b7524446eab7a147e42000015
Thorkee <thorkee@outlook.com>
2021-10-07 18:44:55 +0800

    create a structure for fellows to fill in the code

--- Tutorial1.py
+++ Tutorial1.py
@@ -28,6 +28,17 @@ def function4():
     else:
         print("Sorry my brother, I'm not that advanced to solve your input numbers. Have a nice day anyway!")
 
+def extrafunction5():
+    
+
+def extrafunction6():
+
+
+def extrafunction7():
+
+
+def extrafunction8():
+
 
 print('This is ENG1003'' Week 1 Tutorial Programming Task')
 inp = input('Enter the function number (from 1 to 4) to be executed: ')
@@ -40,5 +51,13 @@ elif inp == '3':
     function3()
 elif inp == '4':
     function4()
+elif inp == '5':
+    extrafunction5()
+elif inp == '6':
+    extrafunction6()
+elif inp == '7':
+    extrafunction7()
+elif inp == '8':
+    extrafunction8()
 else:
     print('There is no function', inp)
