2da9894e441824cd148defc36a00bc933e5a0e7b
QuintinUmi <90885055+QuintinUmi@users.noreply.github.com>
2021-10-07 19:12:05 +0800

    Update Tutorial1.py

--- Tutorial1.py
+++ Tutorial1.py
@@ -33,7 +33,10 @@ def extrafunction5():
     
 
 def extrafunction6():
-    print("pls replace this line with your code")
+    F_n, S_n = 0,0
+    F_n = input("First number: ")
+    S_n = input("Second number: ")
+    print("The sum is "+str(int(F_n) + int(S_n)))
 
 
 def extrafunction7():
