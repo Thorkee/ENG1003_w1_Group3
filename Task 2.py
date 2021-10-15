2c3a849cbf7ce4ecada48d3e3add02b3731635b3
Quintin_Umi <qqj030212@gmail.com>
2021-10-15 18:57:29 +0800

    Improve

--- Task 2.py
+++ Task 2.py
@@ -4,8 +4,8 @@ import math
 
 x_min = 0.0 
 x_max = 100.0
-y_min = -200.0 
-y_max = 180.0
+y_min = 0.0 
+y_max = 100.0
 
 C_T = np.arange(x_min, x_max, 0.02)
 d_T = 5.0
@@ -24,6 +24,10 @@ def func(inp, sel):
     else:
         print("The Function is not exist")
         exit()
+
+def cal_line(inp, CP):
+    CTnCF = d_T * CP[0][0] + d_F * CP[0][1]
+    return (CTnCF - d_T * inp) / d_F
     
 def SfCP():
     #Search for cross point
@@ -35,7 +39,7 @@ def SfCP():
                 if(abs(func(x, i) - func(x, j)) <= 0.02):
                     plt.plot(x, func(x, i), 'o', color = "black")
                     CP.append([x, func(x, i)])
-        x += 0.02
+        x += 0.01
         #if(x >= 39 and x <= 41):
         #    print(func(x, 1) , '\n' , func(x, 2) , '\n' , func(x, 3) , '\n' , func(x, 4) , '\n' , '*')
     
@@ -52,19 +56,35 @@ def main():
     plt.style.use('seaborn-paper')
     plt.grid(True)
 
-    plt.axis([0.0, 100.0, -200.0, 180.0])
+    plt.axis([x_min, x_max, y_min, y_max])
 
-    plt.plot(C_T, func(C_T, 1), color = "r", linestyle = "-", linewidth = 1)
-    plt.plot(C_T, func(C_T, 2), color = "b", linestyle = "-", linewidth = 1)
-    plt.plot(C_T, func(C_T, 3), color = "g", linestyle = "-", linewidth = 1)
-    plt.plot(C_T, func(C_T, 4), color = "gold", linestyle = "-", linewidth = 1)
+    plt.plot(C_T, func(C_T, 1), color = "r", linestyle = "-", linewidth = 1, label = "C_T - C_F <= 30")
+    plt.plot(C_T, func(C_T, 2), color = "b", linestyle = "-", linewidth = 1, label = "-0.5C_T - C_F <= -30")
+    plt.plot(C_T, func(C_T, 3), color = "g", linestyle = "-", linewidth = 1, label = "2C_T - C_F >= 20")
+    plt.plot(C_T, func(C_T, 4), color = "gold", linestyle = "-", linewidth = 1, label = "-4C_T - C_F >= -220")
 
-    plt.text(75, 25, "C_T - C_F <= 30", color = "r", fontsize = 10)
-    plt.text(70, -37, "-0.5C_T - C_F <= -30", color = "b", fontsize = 10)
-    plt.text(75, 110, "2C_T - C_F >= 20", color = "g", fontsize = 10)
-    plt.text(60, -174, "-4C_T - C_F >= -220", color = "gold", fontsize = 10)
 
-    CP = SfCP()
+#    plt.text(75, 38, "C_T - C_F <= 30", color = "r", fontsize = 10)
+#    plt.text(1, 43, "-0.5C_T - C_F <= -30", color = "b", fontsize = 10)
+#    plt.text(63, 96, "2C_T - C_F >= 20", color = "g", fontsize = 10)
+#    plt.text(1, 96, "-4C_T - C_F >= -220", color = "gold", fontsize = 10)
+
+    CP = SfCP() #Search for cross point
+
+#    plt.fill(CP[0], CP[1], CP[2], CP[3], color = "pink", alpha = 0.3)
+
+    #If C_F => ...C_T then use y_max, othervise use y_min
+
+#    plt.fill_between(C_T, func(C_T,1), y_max, color = "r", alpha = 0.5)
+ #   plt.fill_between(C_T, func(C_T,2), y_max, color = "b", alpha = 0.5)
+  #  plt.fill_between(C_T, func(C_T,3), y_min, color = "g", alpha = 0.5)
+   # plt.fill_between(C_T, func(C_T,4), y_min, color = "brown", alpha = 0.5)
+
+    plt.plot(C_T, cal_line(C_T, CP), color = "black", linewidth = 2, label = "Result")
+
+    plt.legend(loc='upper left', bbox_to_anchor=(0.0, 0.95))
+
+    plt.annotate("C_T =" + str(round(CP[0][0], 2)) + "\n" + "C_T =" + str(round(CP[0][1], 2)), xy=(CP[0][0], CP[0][1]), xytext=(4, 60), arrowprops=dict(facecolor='k', headwidth=5, width=1))
 
     plt.show()
 
